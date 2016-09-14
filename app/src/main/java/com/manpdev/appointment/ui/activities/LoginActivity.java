30726df30a3d48cbb7c6d64e7026e349e8bfba24
Manuel Antonio Novoa Proenza <novoa.pro@gmail.com>
2016-09-13 23:40:11 -0400

    feat: Add authentication mechanism for firebase.

--- app/src/main/java/com/manpdev/appointment/ui/activities/LoginActivity.java
+++ app/src/main/java/com/manpdev/appointment/ui/activities/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.manpdev.appointment.ui.activities;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.manpdev.appointment.R;
 import com.manpdev.appointment.databinding.ActivityLoginBinding;
 import com.manpdev.appointment.ui.contract.LoginContract;
@@ -15,33 +29,129 @@ import com.manpdev.appointment.ui.contract.LoginContract;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity  implements LoginContract.View{
-    private static final int REQUEST_READ_CONTACTS = 0;
+public class LoginActivity extends AppCompatActivity  implements LoginContract.View, GoogleApiClient.OnConnectionFailedListener{
+
+    private static final String TAG = "LoginActivity";
+
+    //private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int GOOGLE_SIGN_IN = 345;
+
     private ActivityLoginBinding mViewDataBinding;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mViewDataBinding = (ActivityLoginBinding) DataBindingUtil.setContentView(this, R.layout.activity_login);
+        mViewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        initFirebaseAuth();
+        initGoogleSignIn();
+    }
 
-        mViewDataBinding.button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, AppointmentListActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.mAuth.addAuthStateListener(this.mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (this.mAuthListener != null)
+            mAuth.removeAuthStateListener(this.mAuthListener);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//        if (requestCode == REQUEST_READ_CONTACTS) {
+////            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+////
+////            }
+//        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GOOGLE_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                getCredentialsFromGoogle(account);
             }
         }
+    }
+
+    private void initFirebaseAuth() {
+        this.mAuth = FirebaseAuth.getInstance();
+        this.mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    launchFirstActivity();
+                }
+            }
+        };
+    }
+
+    private void launchFirstActivity() {
+        Intent intent = new Intent(LoginActivity.this, AppointmentListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initGoogleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mViewDataBinding.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+    }
+
+    private void getCredentialsFromGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId() + "/" + acct.getDisplayName());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuthWithCredentials(credential);
+    }
+
+    private void firebaseAuthWithCredentials(AuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
 
